6fe616b7ce577a8162a76a84bed384d82be74513
Elielton20 <elieltonsilvagomes16@gmail.com>
2025-10-20 11:04:04 -0300

    Remove bcrypt hash comparison test

--- backend/teste_hash.js
+++ backend/teste_hash.js
@@ -1,9 +1,1 @@
-const bcrypt = require('bcryptjs');
 
-(async () => {
-  const hash = '$2b$10$7EqJtq98hPqEX7fNZaFWoOa6u5jY1jF9Y0q1YwqfY6a5Q9d/1G7K6';
-  const senha = 'admin123';
-
-  const ok = await bcrypt.compare(senha, hash);
-  console.log(ok);
-})();
